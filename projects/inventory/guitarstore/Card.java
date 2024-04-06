55cde5c550a6b21db152f467949da5084c358224
George BK <george.bekari@gmail.com>
2024-04-06 00:32:42 -0700

    save and push

--- projects/inventory/guitarstore/Card.java
+++ projects/inventory/guitarstore/Card.java
@@ -1,7 +1,7 @@
 package projects.inventory.guitarstore;
 
 public class Card {
-    private String face; // "Ace", "King", ...
+    private String face; // "Ace", "King", ... 2
     private String suit; // "Hearts", "Diamonds", ...
 
     public  Card(String face, String suit)
